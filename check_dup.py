70d104dbdfb6e4e96926d31fbcc423000cdd15c8
Rehtsecp <rehtseput@gmail.com>
2022-05-24 13:58:29 +0200

    Changes in check_dup.py

--- check_dup.py
+++ check_dup.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("final_reviews.csv") 
 
+old_count = df.shape[0]
+
 df = df.drop(["id"], axis=1)
 
 df.drop_duplicates(subset=None, inplace=True)
@@ -9,5 +11,12 @@ df.drop_duplicates(subset=None, inplace=True)
 df.reset_index(inplace=True)
 
 df.columns = ['id','company','opinion','date','rating','source','score','sentiment']
+
+new_count = df.shape[0]
+
+dups = old_count - new_count
+
+if dups <= 0: print('No duplicates found!')
+else: print(f'{dups} duplicates found')
 
 df.to_csv("final_reviews.csv",index=False)
